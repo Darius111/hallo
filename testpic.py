b71a1082648bc3dbc01a81d0521d8a43c28156a9
Darius111 <32675262+Darius111@users.noreply.github.com>
2017-12-12 13:33:32 +0100

    Update testpic.py

--- testpic.py
+++ testpic.py
@@ -1,2 +1,7 @@
 import cv2             #Dies ist die Bildverarbeitungsbibliothek OpenCV
 import numpy as np     #Rechnen mit vielen Zahlen in einem Array (z. B. Bilder)
+
+fn = 'testpic001.png'
+img = cv2.imread(fn) 
+
+print("Dimensionen von img: (Höhe, Breite, Farbkanäle)", img.shape)
